a600f30d2aed7ed2656405fe0cc50da1b2581331
Dimon <darberwar@mail.ru>
2025-03-08 23:57:56 +0300

    Наладил работу с алфавитом и написал функцию по подсчету кол-ва участия каждого символа в сообщении

--- Vigenere_cipher/Vigenere_cipher.cpp
+++ Vigenere_cipher/Vigenere_cipher.cpp
@@ -1,12 +1,24 @@
 ﻿#include "uses_things.h"
 
 
-
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	//std::cout << TYPE_NAME(alphabet[0]) << '\n' << TYPE_NAME(msg[2]) << std::endl;
 	
-	//wcout << alphabet[0];
+	// присваиваем значение функции
+	vector<pair<char, int>> sorted_assembling_values = assembling(); 
+
+	// выводим результат
+	for (const auto& result : sorted_assembling_values) 
+		cout << result.first << ": " << result.second << endl;
+	cout << "\n";
 	
+	// проверка, что сборка значений происходит правильно
+	/*int check = count(msg.begin(), msg.end(), 'п');
+	cout << "Кол-во элементов символа: " << check << "\n\n";*/
+	
+	system("pause");
+
 	return 0;
 }
