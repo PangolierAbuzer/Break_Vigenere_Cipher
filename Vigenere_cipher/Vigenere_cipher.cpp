1271486f98aeb15c440f62a32b7a7bed75ef0761
Dimon <darberwar@mail.ru>
2025-03-14 01:06:24 +0300

    Добавил две функции для расчёта символов ключа и функцию дешифровки сообщения. Завершил расшифровку.

--- Vigenere_cipher/Vigenere_cipher.cpp
+++ Vigenere_cipher/Vigenere_cipher.cpp
@@ -1,41 +1,73 @@
 ﻿#include "uses_things.h"
 
 
-    // проверка, что сборка значений происходит правильно
-	/*double check = double(count(msg.begin(), msg.end(), 'п')) / msg.length();
-	cout << "Кол-во элементов символа: " << check << "\n\n";*/
-
-	//loadDataFromFile(); // используем функцию записи общей частоты из файла
-	//cout << "General IC: \n";
-	//// выводим результат
-	//for (const auto& result : general_sorted_assembling_values) 
-	//	cout << result.first << ": " << result.second << endl;
-	//cout << "\n";
-
-
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 	//std::cout << TYPE_NAME(alphabet[0]) << '\n' << TYPE_NAME(msg[2]) << std::endl;
 
-	// присваиваем значение функции
-	vector<pair<char, double>> sorted_assembling_values = assembling_count(msg);
-	cout << "Predict values of msg: \n";
-	for (const auto& result : sorted_assembling_values)
-		cout << result.first << ": " << result.second << endl;
+	cout << "Индексы совпадения ключей длины k от 2 до 42:\n";
+	find_key_size(msg);
+
+
+	loadDataFromFile(); // используем функцию записи общей частоты каждого символа во всех текстах
+
+	cout << '\n';
+	cout << "Предполагаемая длина ключа: 8\n";
+
+	int k = 8;
+	cout << '\n';
+
+	vector<char> predict_decrypt_symbols = most_predict_symbols(k);
+
+	predict_all_str_k_length(k, predict_decrypt_symbols);
+	str_key[4] = 'ы';
+
+	cout << "\nПредполагаемое ключевое слово: \n" << str_key;
+
+
+	cout << "\n\n";
+	for (int i = 0; i < alphabet.length(); i++)
+		cout << i << setw(3);
+	cout << '\n';
+	for (int i = 0; i < alphabet.length(); i++)
+		cout << alphabet[i] << setw(3);
+
+	cout << "\n\n";
+	
+	string msg_decrypt = vigenere_decrypt(k);
+
+	cout << "Расшифрованное сообщение: \n" << msg_decrypt;
+	
+	cout << "\n\n";
+
+	string str = "";
+	string str_s = "";
+	for (int i = 0; i < msg_decrypt.length(); i++)
+	{
+		str += msg_decrypt[i];
+		str_s += msg[i];
+		if (str.length() == 8)
+		{
+			cout << str << "  " << str_s << endl;
+			str = "";
+			str_s = "";
+		}
+	}
+
 	cout << "\n";
 
-	cout << "Индексы совпадения ключей длинны k от 2 до 30:\n";
-	find_key_size(msg);
-	
-	cout << '\n';
 	system("pause");
 
 	return 0;
 }
 
+// проверка, что сборка значений происходит правильно
+	/*double check = double(count(msg.begin(), msg.end(), 'п')) / msg.length();
+	cout << "Кол-во элементов символа: " << check << "\n\n";*/
 
-// более простая реализация поиска индекса совпадений (но менее точная)
+
+// более простая реализация поиска индекса совпадений (но менее точная, хотя по сути разницы нет)
 /*for (int k = 2; k <= 30; k++)
 {
 	string str = "";
@@ -47,3 +79,5 @@ int main()
 		summ += pow(result.second, 2);
 	cout << k << ' ' << summ << endl;
 }*/
+
+
